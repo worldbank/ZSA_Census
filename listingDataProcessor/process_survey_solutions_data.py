38e61c38ad59abe73235589840dcc52d848e9d33
Dunstan Matekenya <dmatekenya@gmail.com>
2020-02-19 21:42:55 -0500

    updated ListingDataProcessor

--- listingDataProcessor/process_survey_solutions_data.py
+++
@@ -1,7 +0,0 @@
-"""
-This module processes data coming from survey solutions
-"""
-
-import sys
-from pathlib import Path
-import pandas as pd
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ListingDataProcessor',
+    version='0.1',
+    packages=['ListingDataProcessor'],
+    url='',
+    license='',
+    author='dmatekenya',
+    author_email='dmatekenya@worldbankgroup.org',
+    description='Custom package for processing census household listing data',
+    install_requires=['pandas', 'geopandas', 'numpy']
+)
